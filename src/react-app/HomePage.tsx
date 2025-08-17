efd9c821595d6a5c200e8dd709612d35d01c28f7
pillig <parker.illig@gmail.com>
2025-08-17 10:02:20 -0600

    change to mantine & basic content

---
+++ src/react-app/HomePage.tsx
@@ -0,0 +1,20 @@
+import { Typography } from "@mantine/core"
+
+export const HomePage = () => {
+    return (
+        <>
+            <Typography>
+                いらっしゃいませ！
+            </Typography>
+            <Typography>
+                Welcome to Denver Shaberiba!
+            </Typography>
+            <Typography>
+                We are a collection of both native and learning Japanese speakers and learners in the Denver metro region.
+            </Typography>
+            <Typography>
+                We meet every 1st and 3rd Thursday of each month.
+            </Typography>
+        </>
+    )
+}
